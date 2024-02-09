ff5654aed39747b95430176ab121f453f6432714
teresaflaitz <33165671+teresaflaitz@users.noreply.github.com>
2024-02-09 14:49:55 -0500

    Added sketching function. Created new button.

--- sketch.js
+++ sketch.js
@@ -1,14 +1,23 @@
 const container = document.querySelector(".container");
 
-let createGrid = function() {
-    for (let i = 0; i < 16; i++){
+let createGrid = function(size = 16) {
+    
+    //Create and append 16 rows
+    for (let i = 0; i < size; i++){
         const gridRow = document.createElement("div");
         gridRow.className="row";
         container.appendChild(gridRow);
-        for (let j = 0; j < 16; j++){
+        
+        //Create and append 16 pixels to each row
+        for (let j = 0; j < size; j++){
             const gridSquare = document.createElement("div");
             gridSquare.className="pixel";
             gridRow.appendChild(gridSquare);
+            
+
+            gridSquare.addEventListener("mouseover", (event) => {
+                gridSquare.style.backgroundColor = "black";
+            });
         };
     };
     
@@ -16,3 +25,17 @@ let createGrid = function() {
 
 createGrid();
 
+const btn = document.querySelector("#new");
+
+let deleteGrid = function(){
+    let grid = document.querySelectorAll(".row");
+    for (let i = 0; i < grid.length; i++) {
+        grid[i].remove();
+    };
+};
+
+btn.addEventListener("click", (event) => {
+    console.log("Button clicked!");
+    /*deleteGrid();*/
+    /*createGrid();*/
+});
